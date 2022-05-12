2cac8c24dd1262c0c1cc3150adad2e2f2f484909
Martin Parizek <marti.parizek@gmail.com>
2022-05-12 16:56:27 +0200

    done

---
+++ src/components/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import emojipedia from "../emojipedia";
+import Card from "./Card";
+
+function App() {
+
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+      <dl className="dictionary">
+        {emojipedia.map((emoji) => {
+          return (
+            <Card key={emoji.id} emoji={emoji.emoji} name={emoji.name} meaning={emoji.meaning}/>
+          )
+        })}
+      </dl>
+    </div>
+  );
+}
+
+export default App;
